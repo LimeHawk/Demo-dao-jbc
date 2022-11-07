54e355682d4237d153b3956432bbb1d5c86c5315
LimeHawk <kaikelfalcao@gmail.com>
2022-11-06 22:51:29 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.Date;
 import models.entities.Department;
+import models.entities.Seller;
 
 public class Program {
 
@@ -8,7 +10,9 @@ public class Program {
         
         Department dep = new Department(1, "Books");
         
-        System.out.println(dep);
+        Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0, dep);
+        
+        System.out.println(seller);
         
     }
     
